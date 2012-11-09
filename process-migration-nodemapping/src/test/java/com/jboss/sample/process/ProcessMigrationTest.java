33b9cbc2076ebb4fa91c78eff98c22ba022a080f
Siamak Sadeghianfar <ssadeghi@redhat.com>
2012-11-09 17:56:35 +0100

    node mapping working, finally

--- process-migration-nodemapping/src/test/java/com/jboss/sample/process/ProcessMigrationTest.java
+++ process-migration-nodemapping/src/test/java/com/jboss/sample/process/ProcessMigrationTest.java
@@ -12,8 +12,11 @@ import org.drools.io.ResourceFactory;
 import org.drools.logger.KnowledgeRuntimeLogger;
 import org.drools.logger.KnowledgeRuntimeLoggerFactory;
 import org.drools.runtime.StatefulKnowledgeSession;
+import org.drools.runtime.process.NodeInstance;
 import org.drools.runtime.process.ProcessInstance;
+import org.jbpm.workflow.instance.NodeInstanceContainer;
 import org.jbpm.workflow.instance.WorkflowProcessInstanceUpgrader;
+import org.jbpm.workflow.instance.node.JoinInstance;
 import org.junit.Test;
 
 public class ProcessMigrationTest {
@@ -24,46 +27,53 @@ public class ProcessMigrationTest {
 		KnowledgeBase kbase = kbuilder.newKnowledgeBase();
 		StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
 		KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newThreadedFileLogger(ksession, "target/trace", 1000);
-		ksession.addEventListener(new DebugProcessEventListener());
+//		ksession.addEventListener(new DebugProcessEventListener());
 
 		// start process
 		ProcessInstance processInstance = ksession.startProcess("process-1.0");
-		
+
 		// wait
-		sleep(5000);
-		
+		sleep(1000);
+
 		// go back to first converge node
+		System.out.println("Migrating process");
 		Map<String, Long> nodeMapping = new HashMap<String, Long>();
 		nodeMapping.put("3", 8L);
-//		nodeMapping.put("6", 7L);
+		nodeMapping.put("6", 7L);
 		WorkflowProcessInstanceUpgrader.upgradeProcessInstance(ksession, processInstance.getId(), "process-2.0", nodeMapping);
-		
-		sleep(5000);
-		
+
+		// adjust triggered nodes data before converge
+        for (NodeInstance nodeInstance: ((NodeInstanceContainer)processInstance).getNodeInstances()) {
+        	if (nodeInstance instanceof JoinInstance && ((JoinInstance)nodeInstance).getNodeId() == 8) {
+        		((JoinInstance)nodeInstance).getTriggers().put(6L, 1);
+        	}
+        }
+
+		sleep(1000);
+
 		// signal continue
 		System.out.println("Signaling Continue...");
 		ksession.signalEvent("Continue", "", processInstance.getId());
-		
+
 		logger.close();
 	}
 
 	private void sleep(long ms) {
-		System.out.println("Waiting...");
 		try {
 			Thread.sleep(ms);
 		} catch (InterruptedException e) {
 		}
 	}
 
-	private static KnowledgeBuilder createKnowledgeBase(String...bpmnProcesses) throws Exception {
+	private static KnowledgeBuilder createKnowledgeBase(String... bpmnProcesses) throws Exception {
 		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-		
+
 		if (bpmnProcesses != null) {
 			for (String bpmnProcess : bpmnProcesses) {
 				kbuilder.add(ResourceFactory.newClassPathResource(bpmnProcess), ResourceType.BPMN2);
 			}
 		}
-		
+
 		return kbuilder;
 	}
 }
