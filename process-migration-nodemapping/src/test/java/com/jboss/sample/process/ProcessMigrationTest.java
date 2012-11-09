8cf6cae8b630e36401ee13667e064e0069f65956
Siamak Sadeghianfar <ssadeghi@redhat.com>
2012-11-09 16:52:05 +0100

    debug process event listener added

--- process-migration-nodemapping/src/test/java/com/jboss/sample/process/ProcessMigrationTest.java
+++ process-migration-nodemapping/src/test/java/com/jboss/sample/process/ProcessMigrationTest.java
@@ -7,6 +7,7 @@ import org.drools.KnowledgeBase;
 import org.drools.builder.KnowledgeBuilder;
 import org.drools.builder.KnowledgeBuilderFactory;
 import org.drools.builder.ResourceType;
+import org.drools.event.DebugProcessEventListener;
 import org.drools.io.ResourceFactory;
 import org.drools.logger.KnowledgeRuntimeLogger;
 import org.drools.logger.KnowledgeRuntimeLoggerFactory;
@@ -23,18 +24,21 @@ public class ProcessMigrationTest {
 		KnowledgeBase kbase = kbuilder.newKnowledgeBase();
 		StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
 		KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newThreadedFileLogger(ksession, "target/trace", 1000);
+		ksession.addEventListener(new DebugProcessEventListener());
 
 		// start process
 		ProcessInstance processInstance = ksession.startProcess("process-1.0");
 		
 		// wait
-		sleep(1000);
+		sleep(5000);
 		
 		// go back to first converge node
 		Map<String, Long> nodeMapping = new HashMap<String, Long>();
 		nodeMapping.put("3", 8L);
-		nodeMapping.put("6", 7L);
+//		nodeMapping.put("6", 7L);
 		WorkflowProcessInstanceUpgrader.upgradeProcessInstance(ksession, processInstance.getId(), "process-2.0", nodeMapping);
+		
+		sleep(5000);
 		
 		// signal continue
 		System.out.println("Signaling Continue...");
